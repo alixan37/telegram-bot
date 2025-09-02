d2e4acf83370c7fb95191a3c1e6a6a75bc3eadfe
alixan37 <alixanaliyev37@gmail.com>
2025-09-02 19:00:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,11 @@ TOKEN = "8259514020:AAE4TKHp0iopshQpetCLbYqXg2tOFpfEIiA"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start(message):
+def send_welcome(message):
     bot.reply_to(message, "Assalomu alaykum! Bot ishlayapti ✅")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
 bot.polling()
