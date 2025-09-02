fc79de8008af744facd3b715b19e5d1a02d7712a
alixan37 <alixanaliyev37@gmail.com>
2025-09-02 20:10:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,28 @@
-from flask import Flask, request
 import telebot
+from flask import Flask, request
 
 TOKEN = "8259514020:AAE4TKHp0iopshQpetCLbYqXg2tOFpfEIiA"
 bot = telebot.TeleBot(TOKEN)
-
 app = Flask(__name__)
 
-# Oddiy /start komandasi
+# Oddiy handler
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Salom! Men ishlayapman 🚀")
+    bot.reply_to(message, "Assalomu alaykum! Men ishga tushdim ✅")
 
-# Render webhookdan update olishi uchun
-@app.route("/", methods=["POST"])
+# Flask route (webhook uchun)
+@app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-    json_str = request.get_data().decode("UTF-8")
+    json_str = request.get_data().decode('UTF-8')
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
     return "!", 200
 
-# Oddiy test uchun
 @app.route("/")
 def webhook():
-    return "Bot ishlayapti!", 200
+    bot.remove_webhook()
+    bot.set_webhook(url="https://telegram-bot-t2vb.onrender.com/" + TOKEN)
+    return "Webhook set", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
